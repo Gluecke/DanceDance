5e1a9d5e79bf6d51aee96db4d990b560a65a358c
Garrett Luecke <garrett_luecke@hotmail.com>
2015-09-29 19:42:08 -0500

    initial commit

---
+++ main.lua
@@ -0,0 +1,22 @@
+require "CiderDebugger";-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-- Created By: Garrett Luecke
+--
+-- Created On: 9/29/2015
+--
+-- Notes: to deploy to a device, build project with emulator, drag apk file to device, install
+--
+-- Purpose: this app has people dancing when you press a button
+--
+--
+--
+--
+-----------------------------------------------------------------------------------------
+
+
+local widgets = require "widget";
+
+
+
