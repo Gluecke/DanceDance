cd2ee0c69ef5f9b14005d83547dd129eea7fc1d6
Garrett Luecke <garrett_luecke@hotmail.com>
2015-09-29 20:56:04 -0500

    added dancing

--- main.lua
+++ main.lua
@@ -20,36 +20,95 @@ require "CiderDebugger";
 --get libraries
 local widgets = require "widget";
 
-
+------------------------
+--variables
 --get screen dimentions
-local screenx = display.contentWidth;
-local screeny = display.contentHeight;
+local screenX = display.contentWidth;
+local screenY = display.contentHeight;
 
 --options
 local doDanceOptions = {
-                    x = screenx/2,
-                    y = screeny/4,
-                    width = screenx/5,
+                    x = screenX/2,
+                    y = screenY/1.2,
+                    width = screenX/5,
                     label = "Dance!"
                         };
+local danceSheetOptions = {
+	-- Required params
+	frames = {
+	},
 
+	-- content scaling
+	sheetContentWidth = 1024,
+	sheetContentHeight = 1024
+        };
+
+--Generate the frames for the other images. adjusting the y value as necessary.
+--set up all the frames in the image
+local frames = danceSheetOptions.frames;
+local frameCount = 0;
+for frow = 1,15 do
+	for fcol = 1,16 do
+		local element = {
+			x = 64 * (fcol - 1),
+			y = 64 * (frow - 1),
+			width = 64,
+			height = 64
+		}
+		table.insert( frames, element )
+                frameCount = frameCount + 1;
+	end
+end
+
+--spritesheet
+local danceSpriteSheet = graphics.newImageSheet("Images/dancers.png", danceSheetOptions);
+
+--create dance sprite sequence data
+local danceSpriteSeq = {
+                name = "dancing",
+                start = 1,
+                count = frameCount,
+                time = 10000,
+                loopCount = 0, --indefinite loop
+                loopDirection = "forward"
+                }
+
+------------------------
+--display elements
+local danceSpriteAnim = display.newSprite(danceSpriteSheet, danceSpriteSeq);
+
+danceSpriteAnim.x = screenX / 2;
+danceSpriteAnim.y = screenY / 4;
+
+
+
+------------------------
 --functions
 local startDance;
 
+
+------------------------
 --GUI elements
 local doDance = widgets.newButton(doDanceOptions);
 
 
 startDance = function ( event )
     
+    --only play if they hold their finger down
     if(event.phase == "began")
         then
-        print("button was pushed, do dance");
+        danceSpriteAnim:play();
+    end
+    
+    if(event.phase == "ended")
+        then
+        danceSpriteAnim:pause();
     end
     
     
 end
 
 
+------------------------
 --event listeners
 doDance:addEventListener("touch", startDance);
